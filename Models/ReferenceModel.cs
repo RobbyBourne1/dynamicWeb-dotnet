ec5b69f3e56260d1a56d9f71626f51cfb3fe291c
Robby Bourne <robbybourne1@gmail.com>
2017-08-29 16:22:38 -0400

    Wow

--- Models/ReferenceModel.cs
+++ Models/ReferenceModel.cs
@@ -17,26 +17,26 @@ namespace dynamicWeb_dotnet.Models
         public string Website { get; set; }
         public string PostDate { get; set; } = DateTime.Now.ToString();
 
-        public async Task<IEnumerable<ReferenceModel>> Builder()
-        {
-            
+        public async Task<IEnumerable<ReferenceModel>> Builder() =>
+            (await new StreamReader(System.IO.File.Open("comments.csv", FileMode.Open))
+            .ReadToEndAsync())
+            .Split('\n')
+            .Where(w => !String.IsNullOrEmpty(w))
+            .Select(s =>
+            {
+                var _data = s.Split(',');
+                Console.WriteLine(s);
+                return new ReferenceModel
+                {
+                    Message = _data[0],
+                    Name = _data[1],
+                    Email = _data[2],
+                    Website = _data[3],
+                    PostDate = _data[4]
+                };
+            });
 
-            using (var reader = new StreamReader(System.IO.File.Open("comments.csv", FileMode.Open)))
-               {
-                    var user = await reader.ReadToEndAsync();
-                    return user.Split('\n').Where(w => !String.IsNullOrEmpty(w)).Select(s => {
-                        var _data = s.Split(',');
-                        Console.WriteLine(s);
-                        return new ReferenceModel
-                        {Message = _data[0], 
-                        Name = _data[1],
-                        Email = _data[2],
-                        Website = _data[3],
-                        PostDate = _data[4]
-                        };
-                    });
-                }
-           
-        }
+
+        
     }
 }
